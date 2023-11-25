94771fd30873f90fa3c4351c52affbc2af0ea6c7
sam-dude <blendert5@gmail.com>
2023-11-25 14:08:42 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+SPORTS = ["Basketball", "Soccer", "Ultimate Frisbes"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+    return render_template("success.html")
+    
